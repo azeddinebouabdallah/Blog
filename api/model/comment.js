f9d34d449687918ac6f57fbad7f2201034e5c987
AzeddineBouabdallah <didou.pro.5@gmail.com>
2018-06-26 20:03:16 +0100

    Adding the comment routes functionality
    Add the Get, post, Get by id, Patch, Delete routes to Comment route.
    also add the email to the comment SCHEMA

--- api/model/comment.js
+++ api/model/comment.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const commentSchema = mongoose.Schema({
         name: {type: String, required: true},
+        email: {type: String, required: true},
         comment: {type: String, required: true},
         article: {type: mongoose.Schema.Types.ObjectId, ref: 'Article', required: true},
         date: {type: Date, default: Date.now}
--- api/routes/comment.js
+++ api/routes/comment.js
@@ -1,20 +1,107 @@
 const express = require('express');
 const router = express.Router();
 
+const Comment = require('../model/comment');
+
+
+
+// Get all comment of a given Article ID
 router.get('/:articleId', (req, res, next) => {
-   
+
+    // Find the comment by ID
+    Comment.find({
+        article: req.params.articleId
+    })
+    .exec((err, data) => {
+        // Error handeling
+        if (err) {
+            return res.status(400).json({
+                error: 'Something went wrong'
+            });
+        }else {
+            // Send response
+            res.status(200).json(data);
+        }
+    });
 });
 
-router.post('/addComment', (req, res, next) => {
-    
+// Add a comment
+router.post('/', (req, res, next) => {
+    // Set the values of the comment SCHEMA
+    let comment = new Comment({
+        name: req.body.name,
+        email: req.body.email,
+        comment: req.body.comment,
+        article: req.body.articleId
+    });
+
+    // Create the element
+    comment.save((err) => {
+        // Handle Errors
+        if (err) {
+            return res.status(400).json({
+                error: 'Something went wrong'
+            });
+        }else {
+            res.status(200).json({
+                message: 'Comment inserted successfuly'
+            });
+        }
+    });
+
 });
 
+// Get ALL Comments
 router.get('/', (req, res, next) => {
-    
+    Comment.find({}).exec((err, data) => {
+        // Error Handeling
+        if (err) {
+            return res.status(400).json({
+                error: 'Something went wrong'
+            })
+        }else {
+            res.status(200).json(data);
+        }
+    });
 });
 
+// Delete Comment by ID
 router.delete('/:commentId', (req, res, next) => {
-    
+    Comment.findOne({   
+        _id : req.params.commentId
+    }).exec((err, data) => {
+        // Error handeling
+        if (err) {
+            return res.status(400).json({
+                error: 'Something went wrong'
+            });
+        }else {
+            if (data) { 
+                // See if the comment exists
+                Comment.remove({ 
+                    // If So . Delete comment
+                    _id : req.params.commentId
+                }).exec((err) => {
+                    // Error Handeling
+                    if (err) { 
+                        return res.status(400).json({
+                            error: 'Something went wrong'
+                        })
+                    }else {
+                        // Send response
+                        res.status(200).json({
+                            message: 'Comment deleted Successfuly'
+                        })
+                    }
+                });
+            }else { 
+                // If Comment dosen't exist
+                res.status(500).json({
+                    message: 'This comment doesn\'t exist'
+                });
+            }
+        }
+    });
 });
 
 module.exports = router;
